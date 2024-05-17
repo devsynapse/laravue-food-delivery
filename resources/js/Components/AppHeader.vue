56ae9d66ebe44d9b60b09eccb7710c6cc5adcff6
Synapse Dev <synapsesoftdev@gmail.com>
2024-05-17 16:31:57 -0600

    search bar is separate component

--- resources/js/Components/AppHeader.vue
+++ resources/js/Components/AppHeader.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import CartButton from './CartButton.vue'
+import SearchBar from './SearchBar.vue'
 import AppLogo from '../../images/app-logo.jpg'
 
 </script>
@@ -11,35 +12,10 @@ import AppLogo from '../../images/app-logo.jpg'
             <div class="flex flex-wrap items-center justify-between p-4">
                 <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img :src="AppLogo" class="h-8" alt="App Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap">
-                        LaraVue Food</span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap">LaraVue Food</span>
                 </a>
                 <CartButton />
-                <div id="search-form" class="w-full max-w-2xl mx-auto">
-                    <form>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
-                            </div>
-                            <input v-on:keyup="searchProducts" v-model="searchQuery" type="search" id="default-search"
-                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Search products" required />
-                                
-                        </div>
-                        <div v-if="showSearchDropdown" id="dropdown" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-full max-w-2xl absolute">
-                            <ul v-if="searchResults.length > 0" class="py-2 text-sm text-gray-700">
-                                <li v-for="searchResult in searchResults">
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">{{ searchResult.name }}</button>
-                                </li>    
-                            </ul>
-                            <p v-else class="p-4">Nothing found</p>
-                        </div>
-                    </form>
-                </div>    
+                <SearchBar />
             </div>
         </nav>
     </header>
