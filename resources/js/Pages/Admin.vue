dd25793cb3d368bdfa4982bc7dcb26b0c5e5fcf6
Synapse Dev <synapsesoftdev@gmail.com>
2024-09-21 21:16:18 -0400

    order details

--- resources/js/Pages/Admin.vue
+++ resources/js/Pages/Admin.vue
@@ -19,7 +19,8 @@ const fetchOrders = async () => {
         <h2 class="text-center py-6 text-3xl font-extrabold">Orders</h2>
         <div id="orders-list" class="flex flex-col gap-2">
             <div v-for="order in orders" class="bg-white rounded p-4 w-full">
-                <p>Order Id: {{ order.id }} First name: {{ order.first_name }} Last name: {{ order.last_name }} Comment: {{ order.comment }} </p>
+                <p>Order Id: {{ order.id }} First name: {{ order.first_name }} Last name: {{ order.last_name }} Comment: {{ order.comment }} Status: {{ order.status }}</p>
+                <div>Order details</div>
             </div>
         </div>
     </div>
---
+++ resources/js/Pages/Orders.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const orders = ref([])
+
+onMounted(async() => {
+    fetchOrders();
+})
+
+const fetchOrders = async () => {
+    await axios.get('/api/orders')
+        .then(response => orders.value = response.data.data)
+        .catch(error => console.log(error))
+}
+</script>
+
+<template>
+    <div id="order-show">
+        
+    </div>
+</template>
