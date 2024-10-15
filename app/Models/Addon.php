c3946eccac61a8daf51dcf37269ffca640e0e554
Synapse Dev <synapsesoftdev@gmail.com>
2024-10-14 20:49:21 -0400

    products addon model created

---
+++ app/Models/Addon.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Order;
+
+class Addon extends Model
+{
+    use HasFactory;
+}
